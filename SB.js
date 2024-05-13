81eb95a9021d564234e02db56fa9031b879d7b2d
Atemmanut2001 <atemmanut1234@gmail.com>
2024-05-13 22:56:02 +0200

    Add files via upload

---
+++ SB.js
@@ -0,0 +1,10 @@
+function checkAnswer() {
+    var answer = document.getElementById("answer").value.toLowerCase();
+
+//Replace "sunday" with the correct answer
+if(answer === "sunday") {
+    window.location.href = "surprise.html";
+} else {
+    document.getElementById("message").innerHTML = "Inocrrect answer. PLease try again.";
+}
+}
